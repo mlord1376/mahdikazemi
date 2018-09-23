12fb7b65fee89f34c5f743710021dc95b1cb5cbf
mlord1376 <40924147+mlord1376@users.noreply.github.com>
2018-09-23 12:49:45 +0330

    new

---
+++ Stuff/Projects/WebLord/Stuff/script.js
@@ -0,0 +1,46 @@
+﻿        
+            function autoCompleteArea(){
+                
+                $("iframe").contents().find("html").html("<html><head><style type='text/css'>" + $("#cssPanel").val() + "</style></head><body>" + $("#htmlPanel").val() + "</body></html>");
+              
+                document.getElementById("outputPanel").contentWindow.eval($("#javascriptPanel").val());
+            }
+            
+            $(".toggleButton").hover(function(){
+            
+                $(this).addClass("highlighted");
+                
+            }, function(){
+                
+                $(this).removeClass("highlighted");
+                
+            });
+            
+            $(".toggleButton").click(function(){
+                
+                $(this).toggleClass("active");
+                
+                $(this).removeClass("highlighted"); 
+                
+                var panelId = $(this).attr("id") + "Panel";
+                
+                $("#" + panelId).toggleClass("hidden");
+            
+                var numberOfActivePanel = 4 - $(".hidden").length;
+                        
+                $(".panel").width(($(window).width() / numberOfActivePanel ) - 10);
+            });
+            
+        
+            $(".panel").height($(window).height() - $("#header").height() - 5);
+                
+            $(".panel").width(($(window).width() / 2) - 4);
+            
+            autoCompleteArea();
+            
+            $("textarea").on("paste change keyup", function(){
+                
+                autoCompleteArea();
+
+            });
+            
